e8b30f21109475fdb25fac383a8715073fd17ed4
a-wotton <a_wotton@fanshaweonline.ca>
2023-11-05 15:44:23 -0500

    boxed out site layout

--- js/main.js
+++ js/main.js
@@ -88,3 +88,46 @@ hotspot1.addEventListener("mouseover", showSpot1);
 hotspot2.addEventListener("mouseover", showSpot2);
 hotspot3.addEventListener("mouseover", showSpot3);
 
+//xray placeholder 
+
+    //variables
+    let imageCon = document.querySelector("#imageCon"),
+        drag = document.querySelector(".image-drag"),
+        left = document.querySelector(".image-left"),
+        dragging = false,
+        min = 0,
+        max = imageCon.offsetWidth;
+
+        function onDown() {
+            dragging = true;
+            console.log("on Down");
+        }
+
+        function onUp() {
+            dragging = false;
+            console.log("on Up");
+        }
+
+        function onMove(event) {
+            if (dragging===true) {
+                let x = event.clientX - imageCon.getBoundingClientRect().left;
+
+                if (x < min) {
+                    x = min;
+
+                } else if (x > max) {
+                    x = max-4;
+                }
+
+
+                drag.style.left = x + "px";
+                left.style.width = x + "px";    
+        }
+        }
+
+
+        //event listener
+        drag.addEventListener("mousedown", onDown);
+        document.body.addEventListener("mouseup", onUp);
+        document.body.addEventListener("mousemove", onMove);
+
